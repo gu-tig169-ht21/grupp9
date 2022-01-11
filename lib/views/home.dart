d484f00f235a4c9419b6ddda9e28de646bf528cf
caseymaze <cajsa.nilsson92@hotmail.com>
2022-01-11 12:47:31 +0100

    smoothfix

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -12,58 +12,47 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StatefulBuilder(
-        builder: (BuildContext context, StateSetter setState) {
-      return Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/1.jpg"),
-            fit: BoxFit.cover,
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/1.jpg"),
+          fit: BoxFit.cover,
         ),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            extendBody: true,
-            appBar: AppBar(
-              backgroundColor: Colors.black12.withOpacity(0.8),
-              title: const Text(
-                ('Cocktaily'),
-                style: TextStyle(fontSize: 25),
-              ),
-              centerTitle: true,
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          extendBody: true,
+          appBar: AppBar(
+            backgroundColor: Colors.black12.withOpacity(0.8),
+            title: const Text(
+              ('Cocktaily'),
+              style: TextStyle(fontSize: 25),
             ),
-            body: Consumer<CocktailsProvider>(
-                builder: (context, CocktailsProvider data, child) {
-              if (data.rlist.isEmpty) {
-                return const Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text('Loading'),
-                );
-              } else {
-                return Quotes(data.rlist[0]);
-              }
-            }),
+            centerTitle: true,
           ),
+          body: Consumer<CocktailsProvider>(
+              builder: (context, CocktailsProvider data, child) {
+            if (data.rlist.isEmpty) {
+              return const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text('Loading'),
+              );
+            } else {
+              return Quotes(data.rlist[0]);
+            }
+          }),
         ),
-      );
-    });
+      ),
+    );
   }
 }
 
 class Quotes extends StatelessWidget {
-  final list = [
-    '“Different cocktails for different Saturday nights.” ― Drew Barrymore',
-    '“No amount of physical contact could match the healing powers of a well made cocktail.” — David Sedaris',
-    '"Cenosillicaphobia (noun): the fear of an empty glass"',
-    '"When life gives you lemons, make whiskey sours"',
-    '“Shaken, not stirred.” —James Bond',
-  ];
-  final _random = Random();
   final Cocktails cocktail;
 
-  Quotes(this.cocktail, {Key? key}) : super(key: key);
+  const Quotes(this.cocktail, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +61,8 @@ class Quotes extends StatelessWidget {
         child: Column(children: [
           Padding(
             padding: const EdgeInsets.only(top: 6.0),
-            child: Text(list[_random.nextInt(4)],
+            child: Text(
+                Provider.of<CocktailsProvider>(context, listen: false).quote,
                 style: const TextStyle(fontSize: 18)),
           ),
           Padding(
